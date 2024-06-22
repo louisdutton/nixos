3524c2e6c0db0e24c6c4d44d8b642b4d6f10e414
Louis Dutton <louis.dutton@travelchapter.com>
2024-06-22 08:16:41 +0000

    fix zsh vi mode

--- home.nix
+++ home.nix
@@ -32,6 +32,7 @@
 		enable = true;
 		autocd = true;
 		enableCompletion = true;
+
 		shellAliases = {
 			rebuild = "sudo NIX_SSL_CERT_FILE=/etc/ssl/certs/Cloudflare_CA.pem nixos-rebuild switch";
 			config = "sudo nvim /etc/nixos/";
@@ -39,6 +40,35 @@
 			e = "nvim";
 			g = "lazygit";
 		};
+
+		initExtra = ''
+			# vi mode
+			bindkey -v
+			bindkey "^H" backward-delete-char
+			bindkey "^?" backward-delete-char
+			export KEYTIMEOUT=1
+
+			# Change cursor shape for different vi modes.
+			function zle-keymap-select {
+				if [[ $KEYMAP == vicmd ]] ||
+					 [[ $1 = "block" ]]; then
+					echo -ne "\e[1 q"
+				elif [[ $KEYMAP == main ]] ||
+						 [[ KEYMAP == viins ]] ||
+						 [[ KEYMAP = "" ]] ||
+						 [[ $1 = "beam" ]]; then
+					echo -ne "\e[5 q"
+				fi
+			}
+			zle -N zle-keymap-select
+			zle-line-init() {
+					zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+					echo -ne "\e[5 q"
+			}
+			zle -N zle-line-init
+			echo -ne "\e[5 q" # Use beam shape cursor on startup.
+			preexec() { echo -ne "\e[5 q" ;} # Use beam shape cursor for each new prompt.
+		'';
 	};
 
 	programs.lazygit.enable = true;
