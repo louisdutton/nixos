a647de3b91e5a822e501b4cdcdc1f52f72c6d25a
Louis Dutton <louis.dutton@travelchapter.com>
2024-06-22 08:16:41 +0000

    add catppuccin theme to bat

--- home.nix
+++ home.nix
@@ -57,6 +57,21 @@
 
   programs.bat = {
     enable = true;
+    config = {
+      theme = "catppuccin";
+    };
+    themes = {
+      catppuccin = {
+        src = pkgs.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "bat";
+          rev = "d714cc1d358ea51bfc02550dabab693f70cccea0";
+          sha256 = "Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
+        };
+        file = "themes/Catppuccin Mocha.tmTheme";
+      };
+    };
+  };
   };
 
   programs.zoxide = {
